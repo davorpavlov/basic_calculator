bba40eeae8aad4105f247df39e46dd939824825d
davorpavlov <pavlovic.davor91@gmail.com>
2023-02-18 21:36:07 +0100

    Create calc_examples.py

---
+++ calc_examples.py
@@ -0,0 +1,24 @@
+a = 12
+b = 8
+c = 2.5
+
+# zbroj
+print("a + b =", a + b)
+
+# razlika
+print("a - b =", a - b)
+
+# umnožak
+print("a * b =", a * b)
+
+# količnik
+print("a / b =", a / b)
+
+# cijelobrojno dijeljenje
+print("a // b =", a // b)
+
+# ostatak pri dijeljenju
+print("a % b =", a % b)
+
+# potenciranje
+print("a ** c =", round(a ** c, 2))
